3ef91da6977fc0b22bdc0607c5bfdac80c589c64
sahil <sahilpatel.elsner@gmail.com>
2022-04-11 18:02:18 +0530

    add space

--- food/src/components/Cart/Cart.js
+++ food/src/components/Cart/Cart.js
@@ -6,12 +6,12 @@ import CartItem from "./CartItem";
 const Cart = (props) => {
   const cartCtx = useContext(CartContex);
   const totalAmount = ` $${cartCtx.totalAmount.toFixed(2)}`;
-  const hasItems = cartCtx.items.length > 0;
+  const hasItems = cartCtx.items.length > 0;    
   const cartItemRemoveHandler = (id) => {
     cartCtx.removeItem(id);
   };
   const cartItemAddHandler = (item) => {
-    cartCtx.addItem({...item,amount:1}); 
+    cartCtx.addItem({...item,amount:1});     
   };
   const cartItems = (
     <ul className={classes["capricert-items"]}>
